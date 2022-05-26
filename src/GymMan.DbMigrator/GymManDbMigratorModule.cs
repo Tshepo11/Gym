908f4d6df06b9c580cfce9356a446a403890c025
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:15:19 +0200

    Add files via upload

---
+++ src/GymMan.DbMigrator/GymManDbMigratorModule.cs
@@ -0,0 +1,19 @@
+﻿using GymMan.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Modularity;
+
+namespace GymMan.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(GymManEntityFrameworkCoreModule),
+    typeof(GymManApplicationContractsModule)
+    )]
+public class GymManDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+    }
+}
