1f731514f1788736131aa505ee293270d36cc3da
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:04:52 +0200

    Add files via upload

---
+++ src/GymMan.HttpApi/Controllers/GymManController.cs
@@ -0,0 +1,14 @@
+﻿using GymMan.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace GymMan.Controllers;
+
+/* Inherit your controllers from this class.
+ */
+public abstract class GymManController : AbpControllerBase
+{
+    protected GymManController()
+    {
+        LocalizationResource = typeof(GymManResource);
+    }
+}
