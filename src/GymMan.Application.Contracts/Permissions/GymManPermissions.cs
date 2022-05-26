4c7d437a82795e5108dac8f4e9c78288017e8e76
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:17:55 +0200

    Add files via upload

---
+++ src/GymMan.Application.Contracts/Permissions/GymManPermissions.cs
@@ -0,0 +1,9 @@
+﻿namespace GymMan.Permissions;
+
+public static class GymManPermissions
+{
+    public const string GroupName = "GymMan";
+
+    //Add your own permission names. Example:
+    //public const string MyPermission1 = GroupName + ".MyPermission1";
+}
