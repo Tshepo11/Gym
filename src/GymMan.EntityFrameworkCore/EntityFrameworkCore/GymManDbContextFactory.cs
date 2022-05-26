bd647479531fc741329c8db3e7e1273202f722d3
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 15:28:30 +0200

    Add files via upload

---
+++ src/GymMan.EntityFrameworkCore/EntityFrameworkCore/GymManDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace GymMan.EntityFrameworkCore;
+
+/* This class is needed for EF Core console commands
+ * (like Add-Migration and Update-Database commands) */
+public class GymManDbContextFactory : IDesignTimeDbContextFactory<GymManDbContext>
+{
+    public GymManDbContext CreateDbContext(string[] args)
+    {
+        GymManEfCoreEntityExtensionMappings.Configure();
+
+        var configuration = BuildConfiguration();
+
+        var builder = new DbContextOptionsBuilder<GymManDbContext>()
+            .UseSqlServer(configuration.GetConnectionString("Default"));
+
+        return new GymManDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../GymMan.DbMigrator/"))
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
