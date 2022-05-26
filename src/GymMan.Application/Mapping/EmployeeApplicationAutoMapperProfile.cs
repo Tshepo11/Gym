25e430e1684c73ce56f72095c57389d3c297823b
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:20:43 +0200

    Add files via upload

---
+++ src/GymMan.Application/Mapping/EmployeeApplicationAutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GymMan.Entity;
+using GymMan.DTO;
+
+namespace GymMan.Mapping
+{
+    public class EmployeeApplicationAutoMapperProfile : Profile
+    {
+        //Mapping the Employee entity to the EmployeeDto
+        public EmployeeApplicationAutoMapperProfile()
+        {
+            CreateMap<Employee, EmployeeDto>();
+            CreateMap<CreateUpdateEmployeeDto, Employee>();
+        }
+    }
+}
