74a7080d78d8c4b1a2ddad5c82b60c0cb4d4e70c
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 14:40:15 +0200

    Add files via upload

---
+++ src/GymMan.HttpApi.Host/GymManBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace GymMan;
+
+[Dependency(ReplaceServices = true)]
+public class GymManBrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "GymMan";
+}
