e931438012fe73c033b2263ed138856839d506ab
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:11:56 +0200

    Add files via upload

---
+++ src/GymMan.Domain/Settings/GymManSettingDefinitionProvider.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Settings;
+
+namespace GymMan.Settings;
+
+public class GymManSettingDefinitionProvider : SettingDefinitionProvider
+{
+    public override void Define(ISettingDefinitionContext context)
+    {
+        //Define your own settings here. Example:
+        //context.Add(new SettingDefinition(GymManSettings.MySetting1));
+    }
+}
---
+++ src/GymMan.Domain/Settings/GymManSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace GymMan.Settings;
+
+public static class GymManSettings
+{
+    private const string Prefix = "GymMan";
+
+    //Add your own setting names here. Example:
+    //public const string MySetting1 = Prefix + ".MySetting1";
+}
