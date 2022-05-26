548fcca111a177123b7685f8992da42ca0e54354
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:08:44 +0200

    Add files via upload

---
+++ src/GymMan.Domain.Shared/GymManDomainErrorCodes.cs
@@ -0,0 +1,6 @@
+﻿namespace GymMan;
+
+public static class GymManDomainErrorCodes
+{
+    /* You can add your business exception error codes here, as constants */
+}
---
+++ src/GymMan.Domain.Shared/GymManDomainSharedModule.cs
@@ -0,0 +1,58 @@
+﻿using GymMan.Localization;
+using Volo.Abp.AuditLogging;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Identity;
+using Volo.Abp.IdentityServer;
+using Volo.Abp.Localization;
+using Volo.Abp.Localization.ExceptionHandling;
+using Volo.Abp.Modularity;
+using Volo.Abp.PermissionManagement;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.TenantManagement;
+using Volo.Abp.Validation.Localization;
+using Volo.Abp.VirtualFileSystem;
+
+namespace GymMan;
+
+[DependsOn(
+    typeof(AbpAuditLoggingDomainSharedModule),
+    typeof(AbpBackgroundJobsDomainSharedModule),
+    typeof(AbpFeatureManagementDomainSharedModule),
+    typeof(AbpIdentityDomainSharedModule),
+    typeof(AbpIdentityServerDomainSharedModule),
+    typeof(AbpPermissionManagementDomainSharedModule),
+    typeof(AbpSettingManagementDomainSharedModule),
+    typeof(AbpTenantManagementDomainSharedModule)
+    )]
+public class GymManDomainSharedModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        GymManGlobalFeatureConfigurator.Configure();
+        GymManModuleExtensionConfigurator.Configure();
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpVirtualFileSystemOptions>(options =>
+        {
+            options.FileSets.AddEmbedded<GymManDomainSharedModule>();
+        });
+
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Add<GymManResource>("en")
+                .AddBaseTypes(typeof(AbpValidationResource))
+                .AddVirtualJson("/Localization/GymMan");
+
+            options.DefaultResourceType = typeof(GymManResource);
+        });
+
+        Configure<AbpExceptionLocalizationOptions>(options =>
+        {
+            options.MapCodeNamespace("GymMan", typeof(GymManResource));
+        });
+    }
+}
