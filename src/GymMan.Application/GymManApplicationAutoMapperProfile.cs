46e2f6dc50f4cd8ad481cf64ca5b1f6f1af95004
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:20:02 +0200

    Add files via upload

---
+++ src/GymMan.Application/GymManApplicationAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace GymMan;
+
+public class GymManApplicationAutoMapperProfile : Profile
+{
+    public GymManApplicationAutoMapperProfile()
+    {
+        /* You can configure your AutoMapper mapping configuration here.
+         * Alternatively, you can split your mapping configurations
+         * into multiple profile classes for a better organization. */
+    }
+}
