49d953652fc6b6c7d0df19d7843edcac693444a1
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2022-05-26 16:13:00 +0200

    Add files via upload

---
+++ src/GymMan.Domain/Entity/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace GymMan.Entity
+{
+    public class Employee : AuditedAggregateRoot<Guid>
+    {
+        [Key]
+        public Guid EmployeeID { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+        public DateTime DOB { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string JobTitle { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Cellphone { get; set; }
+    }
+}
